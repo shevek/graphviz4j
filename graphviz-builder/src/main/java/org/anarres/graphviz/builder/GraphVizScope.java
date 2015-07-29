329be71e2690d1c96121abcf663d979063443275
Shevek <shevek@anarres.org>
2015-07-28 17:00:16 -0700

    Add default GraphVizScope implementation.

--- graphviz-builder/src/main/java/org/anarres/graphviz/builder/GraphVizScope.java
+++ graphviz-builder/src/main/java/org/anarres/graphviz/builder/GraphVizScope.java
@@ -4,12 +4,19 @@
  */
 package org.anarres.graphviz.builder;
 
+import java.io.Serializable;
+
 /**
  * A tagging interface for things to be used as graphviz scopes.
- * 
+ *
  * You can implement this anywhere; it's just to make reflection easier in the IDE.
  *
  * @author shevek
  */
 public interface GraphVizScope {
+
+    public static class Impl implements GraphVizScope, Serializable {
+
+        private static final long serialVersionUID = 1L;
+    }
 }
